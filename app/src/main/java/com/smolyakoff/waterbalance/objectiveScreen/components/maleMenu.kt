824e1cd1b1d8fad095c4abfb6e7caa2704440e6a
Alexey <103638723+incw@users.noreply.github.com>
2022-08-11 15:35:23 +0700

    fix DropdownMenu

--- app/src/main/java/com/smolyakoff/waterbalance/objectiveScreen/components/maleMenu.kt
+++ app/src/main/java/com/smolyakoff/waterbalance/objectiveScreen/components/maleMenu.kt
@@ -1,14 +1,20 @@
 package com.smolyakoff.waterbalance.objectiveScreen.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 
 @Preview(showBackground = true)
@@ -25,8 +31,13 @@ fun MaleMenu() {
     var size by remember {
         mutableStateOf(Size.Zero)
     }
-    var list = listOf("Мужчина", "Женщина")
+    val list = listOf("Мужчина", "Женщина")
 
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+    Column(Modifier.padding(20.dp)) {
     OutlinedTextField(
         value = select,
         onValueChange = { select = it },
@@ -36,7 +47,7 @@ fun MaleMenu() {
             .onGloballyPositioned { coordinates ->
                 size = coordinates.size.toSize()
             },
-
+        singleLine = true,
         label = { Text(text = "Выберите пол") },
 
         leadingIcon = {
@@ -44,19 +55,28 @@ fun MaleMenu() {
                 imageVector = Icons.Default.Person,
                 null
             )
+        },
+        trailingIcon = {
+            Icon(icon,"contentDescription",
+                Modifier.clickable { expanded = !expanded })
         }
     )
     DropdownMenu(
         expanded = expanded,
         onDismissRequest = { expanded = false },
     ) {
+
         list.forEach { label ->
             DropdownMenuItem(onClick = {
+
                 select = label
                 expanded = false
+
             }) {
+
                 Text(text = label)
             }
         }
     }
+}
 }
--- app/src/main/java/com/smolyakoff/waterbalance/objectiveScreen/objectiveUI.kt
+++ app/src/main/java/com/smolyakoff/waterbalance/objectiveScreen/objectiveUI.kt
@@ -3,12 +3,10 @@ package com.smolyakoff.waterbalance.objectiveScreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.smolyakoff.waterbalance.background.Background
 import com.smolyakoff.waterbalance.objectiveScreen.components.MaleMenu
 
@@ -18,7 +16,7 @@ import com.smolyakoff.waterbalance.objectiveScreen.components.MaleMenu
 fun ObjectiveUI(){
     Background()
     Column(
-        modifier = Modifier.fillMaxSize().padding(10.dp),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
