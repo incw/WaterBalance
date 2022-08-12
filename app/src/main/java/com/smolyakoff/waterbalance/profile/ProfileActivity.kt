a94da3f461444ca6f936b47ab071333fb3d98d1a
Alexey <103638723+incw@users.noreply.github.com>
2022-08-12 15:53:22 +0700

    add profile ui

---
+++ app/src/main/java/com/smolyakoff/waterbalance/profile/ProfileActivity.kt
@@ -0,0 +1,4 @@
+package com.smolyakoff.waterbalance.profile
+
+class ProfileActivity {
+}
---
+++ app/src/main/java/com/smolyakoff/waterbalance/profile/ProfileUI.kt
@@ -0,0 +1,8 @@
+package com.smolyakoff.waterbalance.profile
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileUI(){
+
+}
